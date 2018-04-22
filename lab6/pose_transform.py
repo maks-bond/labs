d5d9be3f825ecc651f068bfd09dba68dc4d7f61f
Maksym Bondarenko <mabondaren@hotmail.com>
2018-04-21 18:46:44 -0700

    Lab 6

--- lab6/pose_transform.py
+++ lab6/pose_transform.py
@@ -9,14 +9,41 @@ import asyncio
 import cozmo
 import numpy
 from cozmo.util import degrees
+from cozmo.util import radians
+import math
 
-def get_relative_pose(object_pose, refrence_frame_pose):
+def get_relative_pose(object_pose, reference_frame_pose):
 	# ####
 	# TODO: Implement computation of the relative frame using numpy.
 	# Try to derive the equations yourself and verify by looking at
 	# the books or slides bfore implementing.
 	# ####
-	return None
+
+	obj_x = object_pose.position.x
+	obj_y = object_pose.position.y
+	obj_z = object_pose.position.z
+	obj_angle = object_pose.rotation.angle_z.radians
+
+	ref_x = reference_frame_pose.position.x
+	ref_y = reference_frame_pose.position.y
+	ref_angle = reference_frame_pose.rotation.angle_z.radians
+
+
+	# 1. transpose to convert the object coordinates to reference coordinates without rotation
+	obj_x_t = obj_x - ref_x
+	obj_y_t = obj_y - ref_y
+
+	# 2. Perform rotation of transposed object coordinates on angle of reference coordinate system.
+	obj_x_n  = math.cos(ref_angle)*obj_x_t + math.sin(ref_angle)*obj_y_t
+	obj_y_n = -math.sin(ref_angle) * obj_x_t + math.cos(ref_angle) * obj_y_t
+
+	# 3. Update angle of the object by just subtracting the angle of reference coordinate system
+	obj_angle_n = obj_angle - ref_angle
+
+	# 4. Create pose object
+	new_obj_pose = cozmo.util.pose_z_angle(obj_x_n, obj_y_n, obj_z, radians(obj_angle_n))
+
+	return new_obj_pose
 
 def find_relative_cube_pose(robot: cozmo.robot.Robot):
 	'''Looks for a cube while sitting still, prints the pose of the detected cube
@@ -33,6 +60,9 @@ def find_relative_cube_pose(robot: cozmo.robot.Robot):
 				print("Robot pose: %s" % robot.pose)
 				print("Cube pose: %s" % cube.pose)
 				print("Cube pose in the robot coordinate frame: %s" % get_relative_pose(cube.pose, robot.pose))
+				print("================================================")
+				#get_relative_pose(cube.pose, robot.pose)
+
 		except asyncio.TimeoutError:
 			print("Didn't find a cube")
 
